fe9c93463d4f26f57bcba01336d7aefc2f01881d
Vincenzo-Palonsky <47026639@est.ort.edu.ar>
2022-06-02 14:30:29 -0300

    Controles del jugador y colisiones

---
+++ Assets/Scripts/camera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position += new Vector3(0, 0, 0.5f);
+    }
+}
---
+++ Assets/Scripts/controller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour
+{
+    public GameObject obstaculo;
+    public int vidas = 3;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position += new Vector3(0,0,0.5f);
+
+        if (Input.GetKeyDown(KeyCode.W) && transform.position.y!=1.5)
+        {
+            transform.position += new Vector3(0, 1.5f, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S) && transform.position.y != -1.5)
+        {
+            transform.position -= new Vector3(0, 1.5f, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A) && transform.position.x != -1.5)
+        {
+            transform.position -= new Vector3(1.5f, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) && transform.position.x != 1.5)
+        {
+            transform.position += new Vector3(1.5f, 0, 0);
+        }
+    }
+
+    void OnCollisionEnter()
+    {
+        vidas--;
+    }
+
+}
