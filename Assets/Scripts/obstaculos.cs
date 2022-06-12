a3f144e26b2c1c666c42504e6016fb3a35cde409
Vincenzo-Palonsky <47026639@est.ort.edu.ar>
2022-06-12 10:24:20 -0300

    A punto de terminar
    Seguimos con los obstáculos, hay que solucionar el problema de que no se mueven

--- Assets/Scripts/obstaculos.cs
+++ Assets/Scripts/obstaculos.cs
@@ -10,21 +10,28 @@ public class obstaculos : MonoBehaviour
     bool arriba = true;
     GameObject[] obstaculos3 = new GameObject[4];
     GameObject[] obstaculos4 = new GameObject[4];
+    public int tiempoCambiar1;
+    public int tiempoCambiar2;
     int j = 0;
+    float positivo = 1.5f;
+    float negativo = -1.5f;
 
     void Start()
     {
+        tiempoCambiar1 = Random.Range(1, 3);
+        tiempoCambiar2 = Random.Range(1, 3);
+
         for (int i = 0; i < 80; i += 10)
         {
             GameObject clon = Instantiate(obstaculo1);
             if (derecha)
             {
-                clon.transform.position = new Vector3(1.5f, 0, i);
+                clon.transform.position = new Vector3(positivo, 0, i);
                 derecha = false;
             }
             else
             {
-                clon.transform.position = new Vector3(-1.5f, 0, i);
+                clon.transform.position = new Vector3(negativo, 0, i);
                 derecha = true;
             }
         }
@@ -34,12 +41,12 @@ public class obstaculos : MonoBehaviour
             GameObject clon = Instantiate(obstaculo2);
             if (arriba)
             {
-                clon.transform.position = new Vector3(0, 1.5f, i);
+                clon.transform.position = new Vector3(0, positivo, i);
                 arriba = false;
             }
             else
             {
-                clon.transform.position = new Vector3(0, -1.5f, i);
+                clon.transform.position = new Vector3(0, negativo, i);
                 arriba = true;
             }
         }
@@ -56,13 +63,13 @@ public class obstaculos : MonoBehaviour
             GameObject clon = Instantiate(obstaculos3[j]);
             if (derecha)
             {
-                clon.transform.position = new Vector3(1.5f, 0, i);
+                clon.transform.position = new Vector3(positivo, 0, i);
                 derecha = false;
                 j++;
             }
             else
             {
-                clon.transform.position = new Vector3(-1.5f, 0, i);
+                clon.transform.position = new Vector3(negativo, 0, i);
                 derecha = true;
                 j++;
             }
@@ -74,21 +81,52 @@ public class obstaculos : MonoBehaviour
             GameObject clon = Instantiate(obstaculos4[j]);
             if (arriba)
             {
-                clon.transform.position = new Vector3(0, 1.5f, i);
-                derecha = false;
+                clon.transform.position = new Vector3(0, positivo, i);
+                arriba = false;
                 j++;
             }
             else
             {
-                clon.transform.position = new Vector3(0, -1.5f, i);
-                derecha = true;
+                clon.transform.position = new Vector3(0, negativo, i);
+                arriba = true;
                 j++;
             }
         }
     }
 
+    public int a;
     void Update()
     {
+        a = Mathf.FloorToInt(Time.time);
 
+        if (a % tiempoCambiar1 == 0)
+        {
+            if (obstaculos3[0].transform.position.x == negativo)
+            {
+                obstaculos3[0].transform.position = new Vector3(positivo, 0, obstaculos3[0].transform.position.z);
+                obstaculos3[2].transform.position = new Vector3(positivo, 0, obstaculos3[2].transform.position.z);
+            }
+
+            else
+            {
+                obstaculos3[0].transform.position = new Vector3(negativo, 0, obstaculos3[0].transform.position.z);
+                obstaculos3[2].transform.position = new Vector3(negativo, 0, obstaculos3[2].transform.position.z);
+            }
+        }
+
+        if (Mathf.FloorToInt(Time.time) % tiempoCambiar2 == 0)
+        {
+            if (obstaculos3[1].transform.position.x == negativo)
+            {
+                obstaculos3[1].transform.position = new Vector3(positivo, 0, obstaculos3[0].transform.position.z);
+                obstaculos3[3].transform.position = new Vector3(positivo, 0, obstaculos3[2].transform.position.z);
+            }
+
+            else
+            {
+                obstaculos3[1].transform.position = new Vector3(negativo, 0, obstaculos3[0].transform.position.z);
+                obstaculos3[3].transform.position = new Vector3(negativo, 0, obstaculos3[2].transform.position.z);
+            }
+        }
     }
 }
